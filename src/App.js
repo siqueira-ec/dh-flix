de1d1132c74b77e0fe56a51b30a3af1b8cba0150
Matheus Siqueira <matheussiqueira.ec@gmail.com>
2020-07-28 15:03:29 -0300

    fix padding top from app wrapper to match nav size

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import Footer from './components/Footer';
 
 const AppWrapper = styled.div`
   background-color: var(--grayDark);
+  padding-top: 94px;
+
+  @media (max-width: 800px) {
+    padding-top: 40px;
+  }
 `;
 
 const App = () => (
